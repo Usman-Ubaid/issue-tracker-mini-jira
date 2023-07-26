6506b126b32cc49ed2b4443693bf0277f21c8a2a
Welcome ! <usmanubaid9@gmail.com>
2023-07-26 17:18:45 +0200

    refactor:convert to ES6 import syntax

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,16 @@
-const express = require("express");
-const sequelize = require("sequelize");
-const dotenv = require("dotenv").config();
-const cookieParser = require("cookie-parser");
+import express from "express";
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-// creating an instance of express application
+dotenv.config();
 const app = express();
 
 // middleware configurations
 app.use(express.json());
-app.use(express.cookieParser());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-// listening to server
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on PORT: ${process.env.PORT}`);
 });
