820f339746f407be280ec783a9543db965d4a339
Welcome ! <usmanubaid9@gmail.com>
2023-08-10 20:03:18 +0200

    feat: add AddIssue.jsx

--- frontend/src/pages/AddIssue.jsx
+++ frontend/src/pages/AddIssue.jsx
@@ -1,7 +1,30 @@
+import { useForm } from "react-hook-form";
 import Layout from "../components/Layout";
 
 const AddIssue = () => {
-  return <Layout></Layout>;
+  const { register, handleSubmit } = useForm();
+  return (
+    <Layout>
+      <form
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}
+        className="add-issue-form"
+      >
+        <h2>Add Issue</h2>
+        <input
+          {...register("title", { required: true })}
+          placeholder="Title of the Issue"
+        />
+        <select {...register("type", { required: true })}>
+          <option>Epic</option>
+          <option>Story</option>
+          <option>Task</option>
+        </select>
+        <input type="submit" className="button" value="Add" />
+      </form>
+    </Layout>
+  );
 };
 
 export default AddIssue;
