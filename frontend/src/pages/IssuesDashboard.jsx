1a1aeed35d71d1ee488dfd358a60f153483bde69
Welcome ! <usmanubaid9@gmail.com>
2023-08-20 16:29:55 +0200

    feat: add table for issues

--- frontend/src/pages/IssuesDashboard.jsx
+++ frontend/src/pages/IssuesDashboard.jsx
@@ -1,7 +1,41 @@
 import Layout from "../components/Layout";
 
 const IssuesDashboard = () => {
-  return <Layout></Layout>;
+  const tableHeadings = [{ id: "Id", title: "Title", type: "Type" }];
+
+  const issues = [
+    { id: 1, issueTitle: "Develop UI", issueType: "Epic" },
+    { id: 2, issueTitle: "Develop API", issueType: "Story" },
+    { id: 3, issueTitle: "Integrate API", issueType: "Task" },
+  ];
+  return (
+    <Layout>
+      <div className="issues-dashboard">
+        <h2>Issues Dashboard</h2>
+        <h3>View All Issues</h3>
+        <table>
+          <thead>
+            {tableHeadings.map((heading) => (
+              <tr>
+                <th>{heading.id}</th>
+                <th>{heading.title}</th>
+                <th>{heading.type}</th>
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {issues.map((issue) => (
+              <tr key={issue.id}>
+                <td>{issue.id}</td>
+                <td>{issue.issueTitle}</td>
+                <td>{issue.issueType}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </Layout>
+  );
 };
 
 export default IssuesDashboard;
