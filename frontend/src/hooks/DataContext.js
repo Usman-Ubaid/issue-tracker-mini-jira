fd4eab1890fc8c3af8132a594aa4d0233dc6b935
Welcome ! <usmanubaid9@gmail.com>
2024-02-04 22:10:10 +0100

    refactor: fetch issues using context

--- frontend/src/hooks/DataContext.js
+++ frontend/src/hooks/DataContext.js
@@ -1,15 +1,40 @@
-import { useState, useContext, createContext } from "react";
+import { useState, useContext, createContext, useEffect } from "react";
+import { fetchData } from "../services/api";
 
 const DataContext = createContext(undefined);
 
 export const DataProvider = ({ children }) => {
   const [data, setData] = useState([]);
+  const [groupedData, setGroupedData] = useState({});
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const fetchedData = await fetchData();
+        setData(fetchedData.data);
+
+        const updatedGroupedData = fetchedData.data.reduce((acc, item) => {
+          const state = item.state;
+          acc[state] = acc[state] || [];
+          acc[state].push(item);
+          return acc;
+        }, {});
+
+        setGroupedData(updatedGroupedData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, [data.length]);
 
   return (
-    <DataContext.Provider value={{ data, setData }}>
+    <DataContext.Provider value={{ data, setData, groupedData }}>
       {children}
     </DataContext.Provider>
   );
 };
 
-export const useData = () => useContext(DataContext);
+export const useData = () => {
+  return useContext(DataContext);
+};
